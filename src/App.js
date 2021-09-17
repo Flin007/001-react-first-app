51d6f24e98e0c4855af16ca135c599fb8c6e56d1
Vladimir <Flin010@inbox.ru>
2021-09-18 00:04:04 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div>
+		<Header />
+		<Technologies />
+		<Footer />
+	</div>
   );
+}
+
+function Header() {
+	return(
+		<header>
+			<ul>
+				<li><a href="">Ссылка1</a></li>
+				<li><a href="">Ссылка2</a></li>
+				<li><a href="">Ссылка3</a></li>
+			</ul>
+		</header>
+	); 
+}
+
+function Technologies() {
+	return(
+		<ul>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JS</li>
+		</ul>
+	)
+}
+
+function Footer() {
+	return(
+		<footer>
+			<p>Footer</p>
+		</footer>
+	)
 }
 
 export default App;
